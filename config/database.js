acecaa6b221f8b8538523dbb8430a0a1302799cb
Iqbal090202 <ialamsyah92@gmail.com>
2022-10-16 15:26:50 +0700

    update 4

--- config/database.js
+++ config/database.js
@@ -1,7 +1,9 @@
 module.exports = {
-  host: "localhost",
-  user: "root",
-  database: "db_users",
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  port: process.env.DB_PORT || "",
+  database: process.env.DB_NAME || "db_users",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
